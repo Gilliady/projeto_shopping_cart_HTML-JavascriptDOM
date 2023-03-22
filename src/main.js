e4c6aad35feae7c50a07ff31389481bb83a1b033
Gilliady <gilliadyazevedo@gmail.com>
2023-03-22 16:32:19 -0300

    finalizado requisito 4

--- src/main.js
+++ src/main.js
@@ -6,15 +6,25 @@ import {
 } from './helpers/shopFunctions';
 import { fetchProductsList /* fetchProduct */ } from './helpers/fetchFunctions';
 
+const productsSection = document.querySelector('.products');
+
+const loading = () => {
+  const loadingText = document.createElement('h3');
+  loadingText.innerText = 'carregando...';
+  loadingText.classList.add('loading');
+  productsSection.appendChild(loadingText);
+};
+
 const renderProduct = async () => {
   const computers = await fetchProductsList('computador');
-  const productsSection = document.querySelector('.products');
+  productsSection.innerHTML = '';
   computers.forEach((computador) => {
     productsSection.appendChild(createProductElement(computador));
   });
 };
 
 window.onload = () => {
+  loading();
   renderProduct();
 };
 
