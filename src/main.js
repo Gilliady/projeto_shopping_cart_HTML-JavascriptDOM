5426483d4758c5dcf95a3e32fed31da11bd21479
Gilliady <gilliadyazevedo@gmail.com>
2023-03-22 16:26:05 -0300

    finalizado requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import {
+  /* createCartProductElement, */
+  createProductElement,
+} from './helpers/shopFunctions';
+import { fetchProductsList /* fetchProduct */ } from './helpers/fetchFunctions';
+
+const renderProduct = async () => {
+  const computers = await fetchProductsList('computador');
+  const productsSection = document.querySelector('.products');
+  computers.forEach((computador) => {
+    productsSection.appendChild(createProductElement(computador));
+  });
+};
+
+window.onload = () => {
+  renderProduct();
+};
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
