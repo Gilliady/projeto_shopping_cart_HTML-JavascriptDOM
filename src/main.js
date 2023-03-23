ae84221f419f9b9d0a03fd859de984ba3a1b8160
Gilliady <gilliadyazevedo@gmail.com>
2023-03-22 22:51:15 -0300

    finalizado requisito 8

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Swal from 'sweetalert2';
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import {
+  createCartProductElement,
   /* createCartProductElement, */
   createProductElement,
 } from './helpers/shopFunctions';
-import { fetchProductsList /* fetchProduct */ } from './helpers/fetchFunctions';
+import { saveCartID } from './helpers/cartFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
 
 const productsSection = document.querySelector('.products');
+const cartSection = document.querySelector('.cart__products');
 
 const loading = () => {
   const loadingText = document.createElement('h3');
@@ -24,19 +27,33 @@ const renderProduct = async () => {
   });
 };
 
-window.onload = () => {
-  loading();
-  renderProduct().catch(() => {
-    document.querySelector('.loading').innerHTML = '';
-    /* h3el.classList.add('error');
-      h3el.classList.remove('loading');
-      h3el.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente'; */
-    // solução com Swal.;
-    Swal.fire({
-      title: 'Algum erro ocorreu, recarregue a página e tente novamente',
-      customClass: { title: 'error' },
+const addEventOnAddOnCartButton = () => {
+  const addOnCartButtons = document.querySelectorAll('.product__add');
+  addOnCartButtons.forEach((button) => {
+    button.addEventListener('click', async () => {
+      const productID = button.parentNode.firstChild.innerText;
+      saveCartID(productID);
+      const productData = await fetchProduct(productID);
+      cartSection.append(createCartProductElement(productData));
     });
   });
+};
+
+window.onload = () => {
+  loading();
+  renderProduct()
+    .then(() => addEventOnAddOnCartButton()).catch(() => {
+      document.querySelector('.loading').innerHTML = '';
+      // solução padrão:
+      /* h3el.classList.add('error');
+      h3el.classList.remove('loading');
+      h3el.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente'; */
+      // solução com Swal:
+      Swal.fire({
+        title: 'Algum erro ocorreu, recarregue a página e tente novamente',
+        customClass: { title: 'error' },
+      });
+    });
 };
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
