4963e4058d9e6e2af4a022f839a5b7406c1d8961
Gilliady <gilliadyazevedo@gmail.com>
2023-03-23 01:31:35 -0300

    tentativa do requisito 11, coisas a se corrigir

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,25 @@
-export const getAddress = () => {
+export const getAddress = async (cep) => {
   // seu código aqui
+  const awesomeApiBaseURL = 'https://cep.awesomeapi.com.br/json/';
+  const brasilAPIBaseURL = 'https://brasilapi.com.br/api/cep/v2/';
+  const cepResponse = await Promise.any([fetch(`${awesomeApiBaseURL}${cep}`),
+    fetch(`${brasilAPIBaseURL}${cep}`)])
+    .then((response) => response.json())
+    .then((data) => data);
+  const { address, district, city, state } = cepResponse;
+  if (!cepResponse.address || !cepResponse.district
+      || !cepResponse.city || !cepResponse.state) {
+    throw new Error();
+  }
+  return `${address} - ${district} - ${city} - ${state}`;
 };
 
-export const searchCep = () => {
+export const searchCep = async () => {
   // seu código aqui
+  try {
+    const result = await getAddress(document.querySelector('.cep-input').value);
+    document.querySelector('.cart__address').innerHTML = result;
+  } catch (error) {
+    document.querySelector('.cart__address').innerHTML = 'CEP não encontrado';
+  }
 };
